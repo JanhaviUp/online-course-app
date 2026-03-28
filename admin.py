588e548ea682589d96554a7b096580e55b464d31
JanhaviUp <124653735+JanhaviUp@users.noreply.github.com>
2026-03-28 13:38:48 +0530

    Register Instructor and Learner in admin site

--- admin.py
+++ admin.py
@@ -3,24 +3,20 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submi
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 2
-
 
 class QuestionInline(admin.TabularInline):
     model = Question
-    extra = 1
-
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
-
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
 
-
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
