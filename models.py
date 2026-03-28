52e04d15925173398e9a2e8eb7f0eaebe09b07ad
JanhaviUp <124653735+JanhaviUp@users.noreply.github.com>
2026-03-28 13:37:14 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,37 +1,14 @@
-
-from django.db import models
-
-class Course(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-
-class Lesson(models.Model):
-    title = models.CharField(max_length=200)
+class Enrollment(models.Model):
+    user = models.CharField(max_length=100)  # simple version
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.choice_text
+        return self.user
 
 
 class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"Submission {self.id} for enrollment {self.enrollment.id}"
